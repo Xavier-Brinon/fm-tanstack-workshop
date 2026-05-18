c5923bef72dea379217c4dea030a499c90f46386
Adam Rackis <adamrackis@hotmail.com>
2026-05-17 21:23:10 -0500

    upgrades - set rsc

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,23 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
+import rsc from "@vitejs/plugin-rsc";
 
 const config = defineConfig({
+  ssr: {
+    // Let Node load `pg` so Vite does not try to resolve optional `pg-native`.
+    external: ["pg"],
+  },
   plugins: [
     nitro({ rollupConfig: { external: [/^@sentry\//] } }),
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      rsc: {
+        enabled: true,
+      },
+    }),
+    rsc(),
     viteReact(),
   ],
 });
